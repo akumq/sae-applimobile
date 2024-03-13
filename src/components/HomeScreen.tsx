09ccf9bc14483e143c3bdcaf8113842e5721b34e
akumq <darkshein65@gmail.com>
2024-03-13 14:50:06 +0100

    Affichage de la requête

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -7,6 +7,7 @@ import { MainStackParamList } from "../NavigationParamList";
 
 import { DepotModel } from '../models/depot.model';
 import { DepotService } from '../services/depot.services';
+import { useEffect, useState } from "react";
 
 type HomeScreenProps = {
     route: RouteProp<MainStackParamList, "One">,
@@ -14,7 +15,21 @@ type HomeScreenProps = {
 };
 
 export function HomeScreen({ navigation }: HomeScreenProps) {
-    const depotsList = DepotService.getDepot()
+    const [depots, setDepots] = useState<DepotModel[]>([]);;
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const depotData: DepotModel[]= await DepotService.getDepot();
+          setDepots(depotData);
+        } catch (error) {
+          console.error("Error fetching depot data:", error);
+        }
+      };
+
+      fetchData();
+    }, []);
+
 
     const cellFactory = (depot: DepotModel) => {
         return <label text={depot.adresse} />
@@ -22,7 +37,7 @@ export function HomeScreen({ navigation }: HomeScreenProps) {
     return (
     <stackLayout height="100%">
         <ListView
-          items={depotsList}
+          items={depots}
           cellFactory={cellFactory}
           separatorColor="transparent"
           height="100%"
--- src/services/depot.services.ts
+++ src/services/depot.services.ts
@@ -1,39 +1,29 @@
-// import { DepotModel } from "../models/depot.model";
-
-// class _DepotService {
-
-//     async getDepot(): Promise<DepotModel[]> {
-
-//         var result : DepotModel[]= [];
-
-//         const response = await fetch("https://ytpaqpikqarnveticqhl.supabase.co/rest/v1/view_depots_details",{
-//             method: "GET",
-//             headers: {
-//                 "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0cGFxcGlrcWFybnZldGljcWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwNDQ3MzUsImV4cCI6MjAxOTYyMDczNX0.4glNGKdXcHAXUyWuO5fpvcmg4oRyH9TvtTZ7OYMkcfc",
-//             },
-//         })
-
-//         if(response.ok){
-//             console.log(response.json())
-//             return [];
-//         }else{
-//             return [];
-//         }
-        
-//         // console.log(result)
-
-//     }
-
-// }
-
-// export const DepotService = new _DepotService()
-
 import { DepotModel } from "../models/depot.model";
-import { useEffect, useState } from "react";
 
 class _DepotService {
-    const [isLoading, setIsLoading] = useState(true);
 
+    async getDepot(): DepotModel[] {
+
+        var result : DepotModel[]= [];
+
+        const response = await fetch("https://ytpaqpikqarnveticqhl.supabase.co/rest/v1/view_depots_details",{
+            method: "GET",
+            headers: {
+                "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0cGFxcGlrcWFybnZldGljcWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwNDQ3MzUsImV4cCI6MjAxOTYyMDczNX0.4glNGKdXcHAXUyWuO5fpvcmg4oRyH9TvtTZ7OYMkcfc",
+            },
+        })
+
+        if(response.ok){
+            // console.log(response.json())
+            result = await response.json()
+            return result;
+        }else{
+            return result;
+        }
+        
+        // console.log(result)
+
+    }
 
 }
 
